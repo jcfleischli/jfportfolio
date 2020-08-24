89175c3618a2ffcadb6bacbd84125a66bce87357
jcfleischli <johnf@loanservicingsoft.com>
2020-08-24 16:22:00 -0700

    adds navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import Container from 'react-bootstrap/Container'
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>jfportfolio</h1>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "John Fleischli",
+      headerLinks: [
+        { title: 'Home', pather: '/'},
+        { title: 'Unreal', pather: '/unreal'},
+        { title: 'Contact', pather: '/contact'},
+      ],
+      home: {
+        title: 'John Fleischli\'s Developer Portfolio',
+        subTitle: 'subTitle text',
+        text: 'Checkout my projects'
+      },
+      unreal: {
+        title: 'Unreal Engine 4 Projects',
+      },
+      contact: {
+        title: 'Contact Me',
+      }
+    }
+  }
+
+  render () {
+    return (
+      <Router>
+        <Container className="p-0" fluid={true}>
+
+          <Navbar className="border-bottom" bg="transparent" expand="lg">
+            <Navbar.Brand>John Fleischli</Navbar.Brand>
+
+            <Navbar.Toggle className="border-0" aria-control="navbar-toggle"/>
+            <Navbar.Collapse id="navbar-toggle">
+              <Nav className="ml-auto">
+                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/unreal">Unreal</Link>
+                <Link className="nav-link" to="/contact">Contact</Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+
+
+
+        </Container>
+      </Router>
+      );
+  }
+
 }
 
 export default App;
